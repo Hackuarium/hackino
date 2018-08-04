b5958158c5aaaf02533f590e98e3c7f47cfb15e5
Luc Patiny <luc@patiny.com>
2018-08-04 17:13:39 +0200

    Reduce memory of Serial thread

--- hack/Serial.h
+++ hack/Serial.h
@@ -13,7 +13,7 @@ void processSpecificCommand(char* data, char* paramValue, Print* output);
 void printSpecificHelp(Print * output);
 
 
-NIL_WORKING_AREA(waThreadSerial, 150); // minimum 128
+NIL_WORKING_AREA(waThreadSerial, 96); // minimum 96
 NIL_THREAD(ThreadSerial, arg) {
   Serial.begin(9600);
   while (true) {
