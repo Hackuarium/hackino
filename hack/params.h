ac7e07374871c470ce343464aaefc83f88b44960
Luc Patiny <luc@patiny.com>
2020-09-05 16:32:26 +0200

    fix: deal with no PARAM_ERROR and PARAM_STATUS

--- hack/params.h
+++ hack/params.h
@@ -1,23 +1,23 @@
 #include <avr/eeprom.h>
 
-#define INT_MAX_VALUE       32767
-#define INT_MIN_VALUE       -32768
-#define LONG_MAX_VALUE      2147483647
+#define INT_MAX_VALUE 32767
+#define INT_MIN_VALUE -32768
+#define LONG_MAX_VALUE 2147483647
 
 // value that should not be taken into account
 // in case of error the parameter is set to this value
-#define ERROR_VALUE  -32768
+#define ERROR_VALUE -32768
 
-#define EE_START_PARAM           0 // We save the parameter from byte 0 of EEPROM
-#define EE_LAST_PARAM            (MAX_PARAM*2-1) // The last parameter is stored at byte 50-51
+#define EE_START_PARAM 0  // We save the parameter from byte 0 of EEPROM
+#define EE_LAST_PARAM \
+  (MAX_PARAM * 2 - 1)  // The last parameter is stored at byte 50-51
 
-#define EE_QUALIFIER             (MAX_PARAM*2)
+#define EE_QUALIFIER (MAX_PARAM * 2)
 
-#define EEPROM_MIN_ADDR            0
-#define EEPROM_MAX_ADDR          1023
+#define EEPROM_MIN_ADDR 0
+#define EEPROM_MAX_ADDR 1023
 
 int parameters[MAX_PARAM];
-
 
 uint16_t getQualifier();
 boolean setParameterBit(byte number, byte bitToSet);
@@ -25,18 +25,20 @@ boolean clearParameterBit(byte number, byte bitToClear);
 void writeLog(uint16_t event_number, int parameter_value);
 
 boolean getParameterBit(byte number, byte bitToRead) {
-  return (parameters[number] >> bitToRead ) & 1;
+  return (parameters[number] >> bitToRead) & 1;
 }
 
 boolean setParameterBit(byte number, byte bitToSet) {
-  if (getParameterBit(number, bitToSet)) return false;
+  if (getParameterBit(number, bitToSet))
+    return false;
   parameters[number] |= 1 << bitToSet;
   return true;
 }
 
 boolean clearParameterBit(byte number, byte bitToClear) {
-  if (! getParameterBit(number, bitToClear)) return false;
-  parameters[number] &=  ~ (1 << bitToClear);
+  if (!getParameterBit(number, bitToClear))
+    return false;
+  parameters[number] &= ~(1 << bitToClear);
   return true;
 }
 
@@ -44,16 +46,15 @@ void toggleParameterBit(byte number, byte bitToToggle) {
   parameters[number] ^= 1 << bitToToggle;
 }
 
-
 void setupParameters() {
-  //We copy all the values in the parameters table
-  eeprom_read_block((void*)parameters, (const void*)EE_START_PARAM, MAX_PARAM * 2);
+  // We copy all the values in the parameters table
+  eeprom_read_block((void*)parameters, (const void*)EE_START_PARAM,
+                    MAX_PARAM * 2);
 }
 
 int getParameter(byte number) {
   return parameters[number];
 }
-
 
 void setParameter(byte number, int value) {
   parameters[number] = value;
@@ -65,10 +66,10 @@ void incrementParameter(byte number) {
 
 void saveParameters() {
   for (byte i = 0; i < MAX_PARAM; i++) {
-    eeprom_write_word((uint16_t*) EE_START_PARAM + i, parameters[i]);
+    eeprom_write_word((uint16_t*)EE_START_PARAM + i, parameters[i]);
   }
-    #ifdef EVENT_LOGGING
- writeLog(EVENT_SAVE_ALL_PARAMETER, 0);
+#ifdef EVENT_LOGGING
+  writeLog(EVENT_SAVE_ALL_PARAMETER, 0);
 #endif
 }
 
@@ -78,25 +79,26 @@ void saveParameters() {
 */
 void setAndSaveParameter(byte number, int value) {
   parameters[number] = value;
-  //The address of the parameter is given by : EE_START_PARAM+number*2
-  eeprom_write_word((uint16_t*) EE_START_PARAM + number, value);
-  #ifdef EVENT_LOGGING
+  // The address of the parameter is given by : EE_START_PARAM+number*2
+  eeprom_write_word((uint16_t*)EE_START_PARAM + number, value);
+#ifdef EVENT_LOGGING
   writeLog(EVENT_PARAMETER_SET + number, value);
 #endif
 }
 
-
-// this method will check if there was a change in the error status and log it in this case
+#ifdef PARAM_ERROR
+// this method will check if there was a change in the error status and log it
+// in this case
 boolean saveAndLogError(boolean isError, byte errorFlag) {
   if (isError) {
-    if (setParameterBit(PARAM_ERROR, errorFlag)) { // the status has changed
+    if (setParameterBit(PARAM_ERROR, errorFlag)) {  // the status has changed
 #ifdef EVENT_LOGGING
       writeLog(EVENT_ERROR_FAILED, errorFlag);
 #endif
       return true;
     }
   } else {
-    if (clearParameterBit(PARAM_ERROR, errorFlag)) { // the status has changed
+    if (clearParameterBit(PARAM_ERROR, errorFlag)) {  // the status has changed
 #ifdef EVENT_LOGGING
       writeLog(EVENT_ERROR_RECOVER, errorFlag);
 #endif
@@ -105,16 +107,14 @@ boolean saveAndLogError(boolean isError, byte errorFlag) {
   }
   return false;
 }
-
-
+#endif
 
 void printParameter(Print* output, byte number) {
   output->print(number);
   output->print("-");
   if (number > 25) {
     output->print((char)(floor(number / 26) + 64));
-  }
-  else {
+  } else {
     output->print(" ");
   }
   output->print((char)(number - floor(number / 26) * 26 + 65));
@@ -127,7 +127,6 @@ void printParameters(Print* output) {
     printParameter(output, i);
   }
 }
-
 
 uint8_t printCompactParameters(Print* output, byte number) {
   if (number > MAX_PARAM) {
--- hack/status.h
+++ hack/status.h
@@ -1,6 +1,9 @@
 
+#ifdef PARAM_ERROR
+
 boolean isError(int filter) {
-  if ((getParameter(PARAM_ERROR) & filter) != 0) return true;
+  if ((getParameter(PARAM_ERROR) & filter) != 0)
+    return true;
   return false;
 }
 
@@ -8,18 +11,24 @@ boolean isError() {
   return (getParameter(PARAM_ERROR) != 0);
 }
 
-boolean getStatus(byte statusFlag) {
-  return getParameterBit(PARAM_STATUS, statusFlag);
-}
+#endif
 
+#ifdef PARAM_ENABLED
 boolean isEnabled(byte statusFlag) {
   return getParameterBit(PARAM_ENABLED, statusFlag);
+}
+#endif
+
+#if defined(PARAM_STATUS) && defined(EVENT_STATUS_ENABLE) && \
+    defined(EVENT_STATUS_DISABLE)
+
+boolean getStatus(byte statusFlag) {
+  return getParameterBit(PARAM_STATUS, statusFlag);
 }
 
 boolean isRunning(byte statusFlag) {
   return getParameterBit(PARAM_STATUS, statusFlag);
 }
-
 
 boolean startProcess(byte statusFlag) {
   boolean statusChanged = setParameterBit(PARAM_STATUS, statusFlag);
@@ -32,7 +41,7 @@ boolean startProcess(byte statusFlag) {
 }
 
 boolean stopProcess(byte statusFlag) {
-  boolean statusChanged =  clearParameterBit(PARAM_STATUS, statusFlag);
+  boolean statusChanged = clearParameterBit(PARAM_STATUS, statusFlag);
 #ifdef EVENT_LOGGING
   if (statusChanged) {
     writeLog(EVENT_STATUS_DISABLE, statusFlag);
@@ -40,3 +49,5 @@ boolean stopProcess(byte statusFlag) {
 #endif
   return statusChanged;
 }
+
+#endif
